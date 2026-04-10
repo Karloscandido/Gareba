94d5ee5309e3bea0e22a6e5419606916dd72d50b
Karloscandido <120978870+Karloscandido@users.noreply.github.com>
2026-04-09 21:10:11 -0400

    Update meu tv.php

--- meu tv.php
+++ meu tv.php
@@ -5,12 +5,15 @@
     "message": "LOUVADO SEJA DEUS...",
     "auth": 1,
     "status": "Active",
+    "exp_date": "1918411061",
     "is_trial": "0",
     "active_cons": "0",
+    "created_at": "1758052661",
     "max_connections": "2",
     "allowed_output_formats": [
       "m3u8",
-      "ts"
+      "ts",
+      "rtmp"
     ]
   },
   "server_info": {
@@ -22,6 +25,8 @@
     "https_port": "443",
     "server_protocol": "http",
     "rtmp_port": "8880",
+    "timestamp_now": 1775783302,
+    "time_now": "2026-04-09 22:08:22",
     "timezone": "America/Sao_Paulo"
   }
 }
